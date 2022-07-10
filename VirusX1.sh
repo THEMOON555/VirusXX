7449abac5947b378b0024d7ab1925edf17b84a13
TheDemon <108716811+THEMOON555@users.noreply.github.com>
2022-07-10 13:25:46 +0800

    Update VirusX1.sh

--- VirusX1.sh
+++ VirusX1.sh
@@ -2,34 +2,30 @@
 #!/bin/sh
 # by DEMONX
 # code warna
-a,m,h,k,b,u,c,p,bn,o = [
-'\033[90m',
-'\033[31m',
-'\033[32m',
-'\033[33m',
-'\033[94m',
-'\033[35m',
-'\033[36m',
-'\033[37m',
-'\033[41m',
-'\033[0m'
-]
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
 
 read -p " Siapa Nama Kaka?: " name;
 clear
 sleep 1
-\033[31mfiglet HY $name
+echo $red"
+figlet HY $name
 sleep 1
-  \033[35m ×××××××××××××××××××××××××××××××××××××××××××××××××"
-    \033[31m Author : DEMONXX"
-    \033[31m Team   : Cyber Hunter Indonesia"
-  \033[35m ×××××××××××××××××××××××××××××××××××××××××××××××××"
+echo $purple" ×××××××××××××××××××××××××××××××××××××××××××××××××"
+echo   $red"Author" : DEMONXX"
+echo   $red"Team"   : Cyber Hunter Indonesia"
+echo $purple" ×××××××××××××××××××××××××××××××××××××××××××××××××"
 echo
     sleep 1
-              \033[37m[2]\033[32m")\033[32m" Masuk Ke Script"
-              \033[37m[3]\033[32m")\033[32m" Join Grup"
-              \033[37m[4]\033[32m")\033[31m" Keluar"
-              \033[35;1m ×××××××××××××××××××××××××××××××××××××××××××××××××"
+         echo $cyan"[2]"$white")$green" Masuk Ke Script"
+         echo $cyan"[3]"$white")"$green" Join Grup"
+         echo $cyan"[4]"$white")"$red" Keluar"
+          echo $purple" ×××××××××××××××××××××××××××××××××××××××××××××××××"
              sleep 1
              read -p "Pilih: " pill
 if [ $pill = 1 ]; then
