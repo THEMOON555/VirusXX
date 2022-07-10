5d2492d42a8f67557127ff9b075423b3ac4a3534
TheDemon <108716811+THEMOON555@users.noreply.github.com>
2022-07-10 14:20:53 +0800

    Update VirusX1.sh

--- VirusX1.sh
+++ VirusX1.sh
@@ -9,20 +9,27 @@ red='\033[31;1m'
 white='\033[37;1m'
 yellow='\033[33;1m'
 
-read -p "Siapa Nama Kaka" : name;
+echo $cyan"
+read -p "Siapa Nama Kaka: "name;
 clear
 sleep 1
 figlet Hay $name
 sleep 1
-          "×××××××××××××××××××××××××××××××××××××××××××××××××"
-           " Author " : "DEMONXX"
-           " Team "   : "Cyber Hunter Indonesia"
-          "×××××××××××××××××××××××××××××××××××××××××××××××××"
+          echo $cyan":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    echo $cyan"::"$yellow"          AUTHOR"$cyan" :"$yellow" DEMONXXX.                         "$cyan"::"
+    echo $cyan"::"$yellow"          TEAM  "$cyan" :"$yellow" Cyber Hunter Indonesia.           "$cyan"::"
+    echo $cyan"::"$yellow"          
+    echo $cyan":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 echo
+          sleep 1
+               echo $purple" GUNAKAN TOOLS INI DENGAN BIJAK"
+                 echo $cyan"°==============================°"
+                   echo
     sleep 1
-                                ("2")" Masuk Ke Script"
-                                ("3")" Join Grup"
-                                ("4")" Keluar"
+         echo $cyan"["$white"01"$cyan"]"$green" Script"
+         echo $cyan"["$white"02"$cyan"]"$green" Grup Wa"
+         echo $cyan"["$white"03"$cyan"]"$red" Exit"
+         echo
                    "×××××××××××××××××××××××××××××××××××××××××××××××××"
              sleep 1
              read -p "Pilih: " pill
@@ -32,7 +39,7 @@ sleep 1
          
     sh login.sh
 fi
-if [ $pill = 2 ]; then
+if [ $pill = 1 ]; then
     echo
     echo "sabar loading"
     git clone https://github.com/THEMOON555/VirusX1
@@ -43,11 +50,11 @@ if [ $pill = 2 ]; then
     sleep 3
     sh download.sh
 fi
-if [ $pill = 3 ]; then
+if [ $pill = 2 ]; then
 xdg-open https://chat.whatsapp.com/H8TdunkkbEWJdnBOKCSiWB
 sh login.sh
 fi
-if [ $pill = 4 ]; then
+if [ $pill = 3 ]; then
 figlet byebye $name
 exit
 fi
